e4d9169e6f7e180520b1e06ce2e7222bf7ec9d67
daviddluci <gliaubertasdeividas@gmail.com>
2025-07-12 19:53:34 +0300

    add to_hash and from_hash to easily handle json files, and getter for getting specific task

--- lib/task_list.rb
+++ lib/task_list.rb
@@ -9,6 +9,14 @@ class TaskList
     @tasks << task
   end
 
+  def get(index)
+      if index.is_a?(Integer) and index > 0 and index <= @tasks.size
+      return @tasks[index - 1]
+    else
+      puts "Value given to mark_done method must be an integer and within range of tasks size."
+    end
+  end
+
   def print
     @tasks.length.times do |i|
       puts "#" + i.to_s + ": " + @tasks[i].title + " | Done?: " + @tasks[i].done.to_s 
@@ -29,5 +37,24 @@ class TaskList
     else 
       puts "Value given to delete method must be an integer and within range of tasks size."
     end
+  end
+
+  def self.from_hash(jsonData)
+    task_list = TaskList.new()
+
+    jsonData.each do |singleEntry|
+      task_list.add(Task.new(singleEntry["title"], singleEntry["description"], singleEntry["done"]))
+    end
+
+    return task_list
+  end
+
+  def to_hash()
+    hashArray = []
+    @tasks.each do |task|
+      hashArray << {"title" => task.title, "description" => task.description, "done" => task.done}
+    end
+
+    return hashArray
   end
 end
