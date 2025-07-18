f3f1c48f0cb55bd66f5b637a1651c0ed04db513c
daviddluci <gliaubertasdeividas@gmail.com>
2025-07-18 21:29:42 +0300

    improve readabilty

--- cli.rb
+++ cli.rb
@@ -41,7 +41,7 @@ class ConsoleBasedUI
 
       case choice
       when "View Tasks Of Previous Days"
-        file = @prompt.ask("\nEnter the date for the Task List in the format of: YYYY-MM-DD.json\n ", required: true)
+        file = @prompt.ask("\nEnter the date for the Task List in the format of: YYYY-MM-DD.json\n", required: true)
         file = "local_data/" + file
 
         if File.exist?(file)
@@ -59,28 +59,28 @@ class ConsoleBasedUI
         puts "\n\n\n"
 
       when "Create new Task List"
-        list = TaskList.new()
-        loop do
-          
-          taskChoice = @prompt.select("\nWhat would you like to do?\n", ["View Tasks", "Add new task", "Mark Task Done", "Delete Task", "Exit (Save)"])
+        list = TaskList.new
 
-          case taskChoice
+        loop do
+          task_choice = @prompt.select("\nWhat would you like to do?\n", ["View Tasks", "Add new task", "Mark Task Done", "Delete Task", "Exit (Save)"])
+
+          case task_choice
           when "View Tasks"
-            clear_console()
-            list.print()
+            clear_console
+            list.print
           when "Add new task"
-            clear_console()
+            clear_console
             title = @prompt.ask("Enter task title: ", required: true)
             description = @prompt.ask("Enter task description: ", required: true)
             list.add(Task.new(title, description))
           when "Mark Task Done"
-            clear_console()
-            list.print()
+            clear_console
+            list.print
             id = @prompt.ask("\nWhich task? ", convert: :int)
             list.mark_done(id)
           when "Delete Task"
-            clear_console()
-            list.print()
+            clear_console
+            list.print
             id = @prompt.ask("\nWhich task? ", convert: :int)
             list.delete(id)
           when "Exit (Save)"
@@ -88,48 +88,58 @@ class ConsoleBasedUI
             break
           end
         end
+      when "Exit"
+        break
       end
     end
   end
-#mongodb+srv://daviddluci:123@cluster0.mmrmrcb.mongodb.net/Tasks?retryWrites=true&w=majority&appName=Cluster0
+
   def remote_mode
-    uriProvided = @prompt.select("\nDid you specify your db uri in .env ?", ["Yes", "No"])
-    uri = ""
-    case uriProvided
+    uri_provided = @prompt.select("\nDid you specify your db uri in .env ?", ["Yes", "No"])
+
+    case uri_provided
     when "No"
       uri = @prompt.ask("Enter the uri for the db: ", required: true)
       client = MongoConnector.client(uri)
-      if client.nil?
-        return
-      end
+      return if client.nil?
     when "Yes"
-      puts "\nConnecting to MongoDB....".yellow()
-      client = MongoConnector.client()
+      puts "\nConnecting to MongoDB....".yellow
+      client = MongoConnector.client
     end
 
     db = client.database
     collection = db[:task_list]
 
     loop do
-      choice = @prompt.select("\nWhat would you like to do?\n", ["View Contents of Remote Database (MongoDB)", "Add new task", "Mark Task Done", "Delete Task", "Exit"])
+      choice = @prompt.select("\nWhat would you like to do?\n", [
+        "View Contents of Remote Database (MongoDB)",
+        "Add new task",
+        "Mark Task Done",
+        "Delete Task",
+        "Exit"
+      ])
 
       case choice
       when "View Contents of Remote Database (MongoDB)"
         collection.find.each do |document|
           puts document
         end
+
       when "Add new task"
         title = @prompt.ask("Title: ", required: true)
         description = @prompt.ask("Description: ", required: true)
-        collection.insert_one({ title: title, description: description, done: false})
+        collection.insert_one({ title: title, description: description, done: false })
+
       when "Mark Task Done"
         title = @prompt.ask("Title: ", required: true)
         result = collection.find_one_and_update({ title: title }, { '$set' => { done: true } })
+
         if result
           puts "Task marked as done.".green
         else
           puts "No task found with title '#{title}'.".red
         end
+
       when "Delete Task"
         title = @prompt.ask("Title: ", required: true)
         result = collection.delete_one({ title: title })
@@ -139,6 +149,7 @@ class ConsoleBasedUI
         else
           puts "No task found with title '#{title}'.".red
         end
+
       when "Exit"
         break
       end
@@ -146,19 +157,19 @@ class ConsoleBasedUI
   end
 
   def start
-    select_mode()
-    @mode == "local" ? local_mode() : remote_mode() 
+    select_mode
+    @mode == "local" ? local_mode : remote_mode
   end
 
   def write_file(list)
-    hashed_list = list.to_hash()
+    hashed_list = list.to_hash
 
-    File.open("local_data/#{DateTime.now().strftime("%Y-%m-%d")}.json", "w") do |file|
+    File.open("local_data/#{DateTime.now.strftime("%Y-%m-%d")}.json", "w") do |file|
       file.write(JSON.pretty_generate(hashed_list))
     end
   end
-
 end
 
+# Start the UI
 ui = ConsoleBasedUI.new
 ui.start
